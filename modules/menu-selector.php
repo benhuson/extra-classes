63ae904ce587837a72693e44b6c14d20738bfa5b
Ben Huson <ben@thewhiteroom.net>
2014-11-29 00:05:01 +0000

    Add Menu Selector class inline documentation.

--- modules/menu-selector.php
+++ modules/menu-selector.php
@@ -26,9 +26,23 @@ add_filter( 'wp_nav_menu_objects', array( 'ExtraClasses_Menu_Selector', 'apply_m
  */
 class ExtraClasses_Menu_Selector {
 
+	/**
+	 * Selected States
+	 *
+	 * @var  array
+	 */
 	protected static $selected_states = array();
+
+	/**
+	 * Deselected States
+	 * 
+	 * @var  array
+	 */
 	protected static $deselected_states = array();
 
+	/**
+	 * Setup Inbuilt States
+	 */
 	public static function setup_inbuilt_states() {
 
 		// Selected States
@@ -45,26 +59,53 @@ class ExtraClasses_Menu_Selector {
 
 	}
 
+	/**
+	 * Register Selected State
+	 *
+	 * States can include formatted placeholders.
+	 * See self::format_current_state().
+	 *
+	 * @param  string  $class  Selected state formatted class.
+	 */
 	public static function register_selected_state( $class ) {
 
 		$class = self::sanitize_state( $class );
 
+		// Only add if not already added
 		if ( ! in_array( $class, self::$selected_states ) ) {
 			self::$selected_states[] = $class;
 		}
 
 	}
 
+	/**
+	 * Register Deselected State
+	 *
+	 * States can include formatted placeholders.
+	 * See self::format_current_state().
+	 *
+	 * @param  string  $class  Deselected state formatted class.
+	 */
 	public static function register_deselected_state( $class ) {
 
 		$class = self::sanitize_state( $class );
 
+		// Only add if not already added
 		if ( ! in_array( $class, self::$deselected_states ) ) {
 			self::$deselected_states[] = $class;
 		}
 
 	}
 
+	/**
+	 * Sanitize State
+	 *
+	 * States are forced to be lowercase with alpahanumeric characters,
+	 * underscores, hyphens and percent signs.
+	 *
+	 * @param   string  $state  State class.
+	 * @return  string          Sanitized state class.
+	 */
 	public static function sanitize_state( $state ) {
 
 		$state = strtolower( $state );
@@ -74,32 +115,66 @@ class ExtraClasses_Menu_Selector {
 
 	}
 
+	/**
+	 * Format Current State
+	 *
+	 * Substitutes placeholder strings with appropriate values.
+	 * May return multiple states a term has ancestors.
+	 *
+	 * - %%$post_type%% : Post type slug
+	 * - %%$taxonomy%%  : Taxonomy slug
+	 * - %%$term%%      : Term slug
+	 *
+	 * @param   state  $state  State class.
+	 * @return  array          Formatted states.
+	 */
 	public static function format_current_state( $state ) {
 
+		// Get the main queried object
 		$qo = get_queried_object();
 
-		// Posts
+		// If state contains a %%post_type%% placeholder...
 		if ( strpos( $state, '%%post_type%%' ) !== false ) {
+
+			// If queried object is a post or this is a post type archive page...
 			if ( is_a( $qo, 'WP_Post' ) || is_post_type_archive() ) {
+
+				// Find and replace post type
 				$post_type = is_post_type_archive() ? get_post_type() : get_post_type( $qo );
 				$state = str_replace( '%%post_type%%', $post_type, $state );
+
 			} else {
+
+				// ... otherwise remove placeholder
 				$state = '';
+
 			}
+
 		}
 
-		// Taxonomies & Terms
+		// If state contains a %%taxonomy%% placeholder...
 		if ( isset( $qo->taxonomy ) && ( strpos( $state, '%%taxonomy%%' ) !== false || strpos( $state, '%%taxonomy%%' ) !== false ) ) {
+
+			// Allow current taxonomy to be filtered
 			$taxonomy = apply_filters( 'extraclasses_menu_selector_current_taxonomy', $qo->taxonomy );
 
+			// Find and replace taxonomy
 			if ( ! empty( $taxonomy ) ) {
 				$state = str_replace( '%%taxonomy%%', $taxonomy, $state );
 
+				// If state contains a %%term%% placeholder...
 				if ( strpos( $state, '%%term%%' ) !== false ) {
+
+					// Allow current term to be filtered
 					$term_object = apply_filters( 'extraclasses_menu_selector_current_term', $qo );
+
 					if ( isset( $term_object->term_id ) && isset( $term_object->slug ) ) {
 						$term_format = $state;
+
+						// Find and replace term
 						$states = array( str_replace( '%%term%%', $term_object->slug, $term_format ) );
+
+						// If term has ancetsors, create additional states using those terms
 						$anc = get_ancestors( $term_object->term_id, $term_object->taxonomy );
 						if ( count( $anc ) > 0 ) {
 							foreach ( $anc as $a ) {
@@ -108,14 +183,21 @@ class ExtraClasses_Menu_Selector {
 							}
 						}
 						$state = $states;
+
 					}
+
 				}
 
 			} else {
+
+				// ... otherwise remove placeholder
 				$state = '';
+
 			}
+
 		}
 
+		// If only one state, needs to return an array
 		if ( ! is_array( $state ) ) {
 			$state = array( $state );
 		}
@@ -124,6 +206,11 @@ class ExtraClasses_Menu_Selector {
 
 	}
 
+	/**
+	 * Get Current Selected States
+	 *
+	 * @return  array  Current selected states.
+	 */
 	public static function get_current_selected_states() {
 
 		$states = array();
@@ -140,6 +227,11 @@ class ExtraClasses_Menu_Selector {
 
 	}
 
+	/**
+	 * Get Current Deselected States
+	 *
+	 * @return  array  Current deselected states.
+	 */
 	public static function get_current_deselected_states() {
 
 		$states = array();
@@ -156,8 +248,18 @@ class ExtraClasses_Menu_Selector {
 
 	}
 
+	/**
+	 * Apply Menu Class Filters
+	 *
+	 * Filters the nav menu objects and adjusts their classes.
+	 *
+	 * @param   array  $sorted_menu_items  Menu items.
+	 * @param   array  $args               Menu item arags.
+	 * @return  array                      Filtered menu items.
+	 */
 	public static function apply_menu_class_filters( $sorted_menu_items, $args ) {
 
+		// Get current states
 		$states = self::get_current_selected_states();
 		$deselected_states = self::get_current_deselected_states();
 
@@ -165,13 +267,13 @@ class ExtraClasses_Menu_Selector {
 		foreach ( $sorted_menu_items as $key => $val ) {
 			$classes = $sorted_menu_items[ $key ]->classes;
 
-			// Manual Deselected states
+			// Remove deselected states
 			$found_states = array_intersect( $deselected_states, $classes );
 			if ( count( $found_states ) > 0 ) {
 				$sorted_menu_items[ $key ]->classes = array_diff( $sorted_menu_items[ $key ]->classes, self::get_selected_classes() );
 			}
 
-			// Manual states
+			// Add selected states
 			$found_states = array_intersect( $states, $classes );
 			if ( count( $found_states ) > 0 ) {
 				$sorted_menu_items[ $key ]->classes[] = 'current-menu-ancestor';
@@ -183,6 +285,11 @@ class ExtraClasses_Menu_Selector {
 
 	}
 
+	/**
+	 * Get Selected Classes
+	 *
+	 * @return  array  Menu selected states.
+	 */
 	public static function get_selected_classes() {
 
 		return array(
